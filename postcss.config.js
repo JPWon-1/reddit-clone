1223d067312cc2d3243f17c883c18dc6d5a78fba
JP Won <rocknzero1@gmail.com>
2023-01-17 02:31:09 +0900

    tailwind, postcss 적용

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "plugins": {
+    "tailwindcss": {},
+    "autoprefixer": {},
+    "postcss-preset-env": {}
+  }
+  // plugins: [
+  //   "tailwindcss","postcss-preset-env"
+  // ]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className='text-5xl font-bold underline'>Hello World!</h1>
+      </div>
+      
+    </>
+  )
+}
