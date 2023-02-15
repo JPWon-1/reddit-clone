ed11cb13a3d8d9823f4ca3455c35ed08c46bd05d
원종필 <jp.won@purple.io>
2023-02-16 00:52:50 +0900

    커뮤니티 만들기 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,39 @@
-import styles from '../styles/Home.module.css'
-
+import Link from "next/link";
+import useSWR from "swr";
+import { Sub } from "../types";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.container}>
-        <h1 className='text-5xl font-bold underline'>Hello World!</h1>
-      </div>
-      
-    </>
-  )
+    const fetcher = async (url: string) => {
+        return await axios.get(url).then((res) => res.data);
+    };
+
+    const address = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/subs/sub/topSubs`;
+    const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
+
+    return (
+        <>
+            <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+                {/* 포스트 리스트 */}
+                <div className="w-full md:mr-3 md:w-8/12">
+                    {/* 사이드바 */}
+                    <div className="hidden w-4/12 ml-3 md:block">
+                        <div className="bg-white border rounded">
+                            <div className="p-4 border-b">
+                                <p className="text-lg font-semibold text-center text-black">상위 커뮤니티</p>
+                            </div>
+
+                            {/* 커뮤니티 리스트 */}
+                            <div></div>
+                            <div className="w-full py-6 text-center">
+                                <Link className="w-full p-2 text-center text-white bg-gray-400 rounded" href="/subs/create">
+                                    커뮤니티 만들기
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
