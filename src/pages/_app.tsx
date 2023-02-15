a8ff3c0d89843c0ca90d53ad162ecf6dfd312d26
원종필 <jp.won@purple.io>
2023-02-15 22:57:27 +0900

    navbar 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
-import Axios from 'axios'
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
-import { AuthProvider } from '../context/auth';
+import Axios from "axios";
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { AuthProvider } from "../context/auth";
+import { useRouter } from "next/router";
+import NavBar from "../components/NavBar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
-  Axios.defaults.withCredentials=true;
-  return <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+    Axios.defaults.withCredentials = true;
+
+    const { pathname } = useRouter();
+    const authRoutes = ["register", "login"];
+    const authRoute = authRoutes.includes(pathname);
+
+    return (
+        <AuthProvider>
+            {!authRoute && <NavBar />}
+            <div className={authRoute ? "" : "pt-12"}>
+                <Component {...pageProps} />
+            </div>
+        </AuthProvider>
+    );
 }
