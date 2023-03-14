0c2a30699e8d5c964967e9d27b26b709b302c29f
JP Won <rocknzero1@gmail.com>
2023-03-15 01:27:56 +0900

    포스트 댓글 생성

--- src/pages/r/[sub]/[identifier]/[slug].tsx
+++ src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -3,13 +3,32 @@ import { Post } from "../../../../types";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import dayjs from "dayjs";
+import { useAuthState } from "../../../../context/auth";
+import { FormEvent, useState } from "react";
+import axios from "axios";
 
 const PostPage = () => {
     const router = useRouter();
     const { identifier, sub, slug } = router.query;
+    const { authenticated, user } = useAuthState();
+    const [newComment, setNewComment] = useState("");
 
     const { data: post, error } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
+    const handleSubmit = async (e:FormEvent) => {
+        e.preventDefault();
+        if(newComment.trim() === "") {
+            return;
+        }
 
+        try {
+            await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
+                body: newComment
+            });
+            setNewComment("");
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="flex max-w-5xl px-4 pt-5 mx-auto">
             <div className="w-full md:mr-3 md:w-8/12">
@@ -48,6 +67,58 @@ const PostPage = () => {
                                     </div>
                                 </div>
                             </div>
+                            <div>
+                                {/* 댓글 작성 구간 */}
+                                <div className="pr-6 mb-4">
+                                    {authenticated ? 
+                                        // 로그인이 되었을 때
+                                        (
+                                            <div>
+                                                <p className="mb-1 text-xs">
+                                                    <Link 
+                                                        className="font-semibold text-blue-500"
+                                                        href={`/u/${user?.username}`}
+                                                    >
+                                                        {user?.username}
+                                                    </Link>
+                                                    {" "}으로 댓글 작성
+                                                </p>
+                                                <form onSubmit={handleSubmit}>
+                                                    <textarea 
+                                                        className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:border-gray-600"
+                                                        onChange={e => setNewComment(e.target.value)}
+                                                        value={newComment}
+                                                    ></textarea>
+                                                    <div className="flex justify-end">
+                                                        <button
+                                                            className="px-3 py1 text-white bg-gray-400 rounded"
+                                                            disabled={newComment.trim() === ""}
+                                                        >
+                                                            댓글 작성
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        ) : 
+                                        // 로그인이 안되었을 때
+                                        (
+                                            <div className="flex items-center justify-between px-2 py-4 border border-gray-200">
+                                                <p className="font-semibold text-gray-400">
+                                                    댓글 작성을 위해서 로그인 해주세요
+                                                </p>
+                                                <div>
+                                                    <Link href={`/login`}
+                                                        className="px-3 py-1 text-white bg-gray-400 rounded"
+                                                    >
+                                                        로그인
+                                                    </Link>
+                                                </div>
+                                            </div>
+
+                                        )}
+                                </div>
+                            </div>
+
                         </>
                     )}
                 </div>
