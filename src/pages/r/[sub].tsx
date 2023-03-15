816bbe8909729f987fd743374411819b567db6e3
JP Won <rocknzero1@gmail.com>
2023-03-16 02:17:13 +0900

    포스트 카드 생성

--- src/pages/r/[sub].tsx
+++ src/pages/r/[sub].tsx
@@ -1,41 +1,47 @@
 import axios from 'axios'
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
-import { useAuthState } from '../../context/auth';
+import PostCard from '../../components/PostCard';
 import SideBar from '../../components/SideBar';
+import { useAuthState } from '../../context/auth';
+import { Post } from '../../types';
 
 const SubPage = () => {
   const [ownSub, setOwnSub] = useState(false);
   const { authenticated, user } = useAuthState();
-
   const fileInputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
   const subName = router.query.sub;
-  const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null);
+  const { data: sub, error, mutate } = useSWR(subName ? `/subs/${subName}` : null);
   useEffect(() => {
     if (!sub || !user) return;
     setOwnSub(authenticated && user.username === sub.username);
   }, [sub])
-
+  console.log('sub', sub);
   const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) return;
+
     const file = event.target.files[0];
+    console.log('file', file);
+
     const formData = new FormData();
     formData.append("file", file);
     formData.append("type", fileInputRef.current!.name);
 
     try {
       await axios.post(`/subs/${sub.name}/upload`, formData, {
-        headers: { "Context-type": "multipart/form-data" },
+        headers: { "Context-Type": "multipart/form-data" }
       });
-    } catch (error: any) {
+    } catch (error) {
       console.log(error);
     }
-
   }
+
   const openFileInput = (type: string) => {
+
     const fileInput = fileInputRef.current;
     if (fileInput) {
       fileInput.name = type;
@@ -43,6 +49,17 @@ const SubPage = () => {
     }
   }
 
+  let renderPosts;
+  if (!sub) {
+    renderPosts = <p className="text-lg text-center">로딩중...</p>
+  } else if (sub.posts.length === 0) {
+    renderPosts = <p className="text-lg text-center">아직 작성된 포스트가 없습니다.</p>
+  } else {
+    renderPosts = sub.posts.map((post: Post) => (
+      <PostCard key={post.identifier} post={post} subMutate={mutate} />
+    ))
+  }
+  console.log('sub.imageUrl', sub?.imageUrl)
   return (
     <>
       {sub &&
@@ -50,7 +67,7 @@ const SubPage = () => {
           <div>
             <input type="file" hidden={true} ref={fileInputRef} onChange={uploadImage} />
             {/* 배너 이미지 */}
-            <div className='bg-gray-400'>
+            <div className="bg-gray-400">
               {sub.bannerUrl ? (
                 <div
                   className='h-56'
@@ -58,37 +75,38 @@ const SubPage = () => {
                     backgroundImage: `url(${sub.bannerUrl})`,
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover',
-                    backgroundPosition: 'center'
+                    backgroundPosition: 'center',
                   }}
                   onClick={() => openFileInput("banner")}
                 >
                 </div>
               ) : (
-                <div className='h-20 bg-gray-400'></div>
-
+                <div className='h-20 bg-gray-400'
+                  onClick={() => openFileInput("banner")}
+                ></div>
               )}
             </div>
             {/* 커뮤니티 메타 데이터 */}
             <div className='h-20 bg-white'>
               <div className='relative flex max-w-5xl px-5 mx-auto'>
-                <div className='absoulte' style={{ top: -15 }}>
+                <div className='absolute' style={{ top: -15 }}>
                   {sub.imageUrl && (
                     <Image
                       src={sub.imageUrl}
-                      alt=""
+                      alt="커뮤니티 이미지"
                       width={70}
                       height={70}
-                      className='rounded-full'
+                      className="rounded-full"
                       onClick={() => openFileInput("image")}
                     />
                   )}
                 </div>
                 <div className='pt-1 pl-24'>
                   <div className='flex items-center'>
-                    <h1 className='text-3xl font-bold'>{sub.title}</h1>
+                    <h1 className='text-3xl font-bold '>{sub.title}</h1>
                   </div>
-                  <p className='text-small font-bold text-gray-400'>
-                    /r/{sub.name}
+                  <p className='font-bold text-gray-400 text-small'>
+                    /{sub.name}
                   </p>
                 </div>
               </div>
@@ -96,8 +114,8 @@ const SubPage = () => {
           </div>
           {/* 포스트와 사이드바 */}
           <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
-            <div className='w-full md:mr-3 md:w-8/12'></div>
-            <SideBar sub={sub}/>
+            <div className="w-full md:mr-3 md:w-8/12">{renderPosts} </div>
+            <SideBar sub={sub} />
           </div>
         </>
       }
@@ -105,4 +123,4 @@ const SubPage = () => {
   )
 }
 
-export default SubPage;
+export default SubPage
